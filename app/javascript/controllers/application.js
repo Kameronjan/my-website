cdf5619e4fbc45a65c4ab4445d284fb1d21085fe
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-06-28 16:23:28 -0400

    forgot to push

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,7 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from 'stimulus'
+import { ScrollTo } from 'stimulus-scroll-to'
 
 const application = Application.start()
+application.register("scroll-to", ScrollTo)
 
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+// ... other JavaScript code for your application
